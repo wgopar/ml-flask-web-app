07a83fc9227971fdee7f5afad3a81a10fdba740f
William Mendoza Gopar <mendozawilliam92@gmail.com>
2019-08-22 16:17:02 -0700

    small change

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
-"""
-
-Activate virtual environment that contains the packages in
-requirements.txt
-
-
-"""
-
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def first_func():
-    return 'Hello World'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
