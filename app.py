2e7340deba912c670853cff7c333432bcefcdc86
William Mendoza Gopar <mendozawilliam92@gmail.com>
2019-09-01 19:37:12 -0700

    add prediction endpoint

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, jsonify
 from utils import clean_text
 import pickle
+import time
 import os
 
 app = Flask(__name__)
 
+MODEL_VERSION = 'model_V0.pkl'
+VECTORIZER_VERSION = 'vectorizer_V0.pkl'
+
+# load model assets
+vectorizer_path = os.path.join(os.getcwd(), 'model_assets', VECTORIZER_VERSION)
+model_path = os.path.join(os.getcwd(), 'model_assets', MODEL_VERSION)
+vectorizer = pickle.load(open(vectorizer_path, 'rb'))
+model = pickle.load(open(model_path, 'rb'))
+
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
+def predict():
     """ Main webpage with user input through form and prediction displayed
 
     :return: main webpage host, displays prediction if user submitted in text field
@@ -15,27 +25,34 @@ def home():
 
     if request.method == 'POST':
 
-        # prepare input text
         response = request.form['text']
         input_text = clean_text(response)
-
-        # load vectorizer and transform input text
-        vectorizer_path = os.path.join(os.getcwd(), 'model_assets', 'vectorizer_test_v.0.0.pkl')
-        vectorizer = pickle.load(open(vectorizer_path, 'rb'))
         input_text = vectorizer.transform([input_text])
-
-        # load model
-        model_path = os.path.join(os.getcwd(), 'model_assets', 'model_test_v.0.0.pkl')
-        model = pickle.load(open(model_path, 'rb'))
-
-        # predict
         prediction = model.predict(input_text)
         prediction = 'Cyber-Troll' if prediction[0] == 1 else 'Non Cyber-Troll'
-
         return render_template('index.html', text=prediction, submission=response)
 
     if request.method == 'GET':
         return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict_api():
+    """ endpoint for model queries (non gui)
+
+    :return: json, model prediction and response time
+    """
+    start_time = time.time()
+
+    request_data = request.json
+    input_text = request_data['data']
+    input_text = clean_text(input_text)
+    input_text = vectorizer.transform([input_text])
+    prediction = model.predict(input_text)
+    prediction = 'Cyber-Troll' if prediction[0] == 1 else "Non Cyber-Troll"  # post processing
+
+    response = {'prediction': prediction, 'response_time': time.time() - start_time}
+    return jsonify(response)
 
 
 if __name__ == '__main__':
